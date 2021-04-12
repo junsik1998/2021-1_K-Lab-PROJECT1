6efbc6fa19d4705cb56fff475863dcde24fa0bbc
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-04-12 22:13:42 +0900

    RN[2] : Change name "Navigator" -> "MainNavigator"

--- App/Navigator/MainNavigator.js
+++ App/Navigator/MainNavigator.js
@@ -1,24 +1,21 @@
 import * as React from "react";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 import ActivityScreen from "../Screens/ActivityScreen";
 import InfoScreen from "../Screens/InfoScreen";
 import SNSScreen from "../Screens/SNSScreen";
-import ActivityTable from "../Screens/ActivityTable"
+import ActivityTable from "../Screens/ActivityTable";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default function Navigator() {
   return (
-    <NavigationContainer>
-    
-      <Tab.Navigator>
-        <Tab.Screen name="Info" component={InfoScreen} />
-        <Tab.Screen name="Activities" component={ActivityScreen} />
-        <Tab.Screen name="SNS" component={SNSScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Info" component={InfoScreen} />
+      <Tab.Screen name="Activities" component={ActivityScreen} />
+      <Tab.Screen name="SNS" component={SNSScreen} />
+    </Tab.Navigator>
   );
 }
