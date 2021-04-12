7c9a71490ae5ca23d7f10e7167dffc596d59571d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-04-12 22:18:07 +0900

    RN[7] : Combine "SignNavigator" & "MainNavigator"

--- App/App.js
+++ App/App.js
@@ -1,21 +1,5 @@
-import * as React from "react";
-import { View } from 'react-native';
-import Navigator from "./Navigator/Navigator";
-import StackNavigator from "./Navigator/StackNavigator"
-import SignNavigator from "./Navigator/SignNavigator"
+import React from "react";
+import SignNavigator from "./Navigator/SignNavigator";
 export default function App() {
-  var signIn = false
-
-  if(signIn == true){
-    return(
-        <Navigator />  
-    ); 
-  }
-  else{
-    return(
-      <SignNavigator/>
-    )
-  }
+  return <SignNavigator />;
 }
-
-
