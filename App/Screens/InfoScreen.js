8b98b496abde6a6d39458ada38377579ea96b567
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-04-12 22:16:50 +0900

    RN[6] : Add Image Viewer

--- App/Screens/InfoScreen.js
+++ App/Screens/InfoScreen.js
@@ -1,51 +1,57 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function InfoScreen() {
   return (
     <View style={styles.container}>
-        <View 
-        title = "Garden title"
-        style = {{
-          flex:2,
-          alignItems :"center", justifyContent:"center",
-            margin : 5,
-            padding : 15,
-            borderColor : "grey",
-            borderWidth : 1,
-            borderRadius : 5,
-        }}>
-          <Text>
-            Garden Title
-          </Text>
-        </View>
-        <View
-        title = "Garden Position"
-        style = {{
-          flex:1,
-          alignItems :"center", justifyContent:"center",
-            margin : 5,
-            padding : 15,
-            borderColor : "grey",
-            borderWidth : 1,
-            borderRadius : 5,
-        }}>
-            <Text>Garden Position</Text>
-        </View>
-        <View
-        title = "Garden Image"
-        style = {{
-          flex : 5,
-          alignItems :"center", justifyContent:"center",
-            margin : 5,
-            padding : 15,
-            borderColor : "grey",
-            borderWidth : 1,
-            borderRadius : 5,
+      <View
+        title="Garden title"
+        style={{
+          flex: 2,
+          alignItems: "center",
+          justifyContent: "center",
+          margin: 5,
+          padding: 15,
+          borderColor: "grey",
+          borderWidth: 1,
+          borderRadius: 5,
         }}
-        >
-          
-        </View>
+      >
+        <Text>Garden Title</Text>
+      </View>
+      <View
+        title="Garden Position"
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          margin: 5,
+          padding: 15,
+          borderColor: "grey",
+          borderWidth: 1,
+          borderRadius: 5,
+        }}
+      >
+        <Text>Garden Position</Text>
+      </View>
+      {/* <View
+        title="Garden Image"
+        style={{
+          flex: 5,
+          alignItems: "center",
+          justifyContent: "center",
+          margin: 5,
+          padding: 15,
+          borderColor: "grey",
+          borderWidth: 1,
+          borderRadius: 5,
+        }}
+      > */}
+      <Image
+        style={{ flex: 5, resizeMode: "contain" }}
+        source={require("../assets/garden.jpeg")}
+      />
+      {/* </View> */}
     </View>
   );
 }
