04e5a1561586f90a4a31fc80085b0981883f131c
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-06-02 01:06:36 +0900

    Android[16] : Add select park & change info

--- AndroidApp/app/src/main/java/com/example/gardening/InfoFragment.kt
+++ AndroidApp/app/src/main/java/com/example/gardening/InfoFragment.kt
@@ -15,8 +15,13 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 
 class InfoFragment : Fragment(), OnMapReadyCallback {
+    var parkArr = arrayOf(ParkData(LatLng(37.5445834335377, 127.03743166984063),"Seoul Forest Park", "Seoul Forest is a park located around 685 Seongsu-dong 1-ga, Seongdong-gu, Seoul. Originally a golf course and racecourse, it was completed in June 2005 with an investment of about 235.2 billion won to provide a resting space for citizens. The area is 1,156,498m2 (about 350,000 pyeong) and consists of 5 theme parks. The Han River and Jungnangcheon Stream flow near the park. It is the third largest park in Seoul after World Cup Park in Mapo-gu (1 million pyeong) and Olympic Park in Songpa-gu (500,000 pyeong).\nSeoul Forest consists of four distinctive spaces: Culture and Arts Park, Experience Learning Center, Ecological Forest, and Wetland Ecology Garden.\nIn addition, Seoul Forest Park is the first park with citizen participation from creation to program operation, and since November 2016, Seoul Green Trust Seoul Forest Conservancy has been responsible for the private operation of the park."),
+                            ParkData(LatLng(37.54956150027909, 127.0818336396154),"Seoul Children's Grand Park", "Children's Grand Park is a resting space for families located in Neung-dong, Gwangjin-gu, Seoul, South Korea. It was opened on Children's Day on May 5, 1973. At about 530,000 square meters, it was the largest in Asia at the time of its opening. It is composed of green forest and green grass, and serves as a public park as a resting and cultural space for not only children, but also adolescents and general citizens. In addition, it functions as a learning and education center equipped with liberal arts facilities, a zoo, a botanical garden, and amusement facilities."),
+                            ParkData(LatLng(37.551390337929895, 126.99099446067123),"Namsan Park", "Namsan is a symbol of Seoul located in the center of Seoul. Its height is 265m above sea level, and its original name was Ingyeongsan, but after King Taejo of the Joseon Dynasty moved the capital from Gaeseong to Seoul in 1394 by Pungsu geography, it was named 'Namsan' because it is a mountain in the south. It is an important mountain as Sangan. In order to hold a ritual to pray for the peace of the country, a shrine was built to enshrin the spirit of the mountain, and it was called Mokmyeok Shrine because it enshrined the mountain god of King Mokmyeok.\n" + "It was designated as a park on March 12, 1940 and opened on September 10, 1968. For 8 years from 1991, 89 negative facilities in the park were relocated and an outdoor botanical garden was created. Hoehyeon-dong and Hannam-dong, Yongsan-gu, have been extensively restored and maintained to provide rest and clean air to citizens, and this park has been reborn as a center of leisure life."),
+                            ParkData(LatLng(37.50781156469194, 126.99278932936299),"Banpo Hangang Park", "Banpo Hangang Park is the first result of the Hangang Renaissance Project and is a park installed near Banpo Bridge. It consists of Jamsu Bridge, Sevit Island, Moonlight Rainbow Fountain, Moonlight Square, Seorae Island, Marina, Banpo Water Taxi Stand, and a picnic area."),
+                            ParkData(LatLng(37.42643316373722, 127.0227470918769),"Seoul Grand Park","Seoul Grand Park is a park located at 102 Grand Park Gwangjang-ro, Gwacheon-si, Gyeonggi-do, Republic of Korea. There is the largest zoo and botanical garden in Korea, and Seoul Land, an amusement park. It is located in Gwacheon, Gyeonggi-do, but the facility operation and management is in charge of Seoul Metropolitan Government.")
+        )
 
-    var locArr =  arrayOf(LatLng(53.37503235738614, 6.173195461374293),LatLng(37.429347415772234, 127.01603054100286))
     var binding :FragmentInfoBinding ? = null
     var select :Int? = null
 
@@ -27,10 +32,6 @@ class InfoFragment : Fragment(), OnMapReadyCallback {
         binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
         binding!!.infoMap.onCreate(savedInstanceState)
         binding!!.infoMap.getMapAsync(this)
-
-
-
-
         return binding!!.root
     }
 
@@ -38,11 +39,16 @@ class InfoFragment : Fragment(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         select = arguments?.getString("select")?.toInt()
         Log.i("TEST", "num : ${select.toString()}")
+
     }
 
     override fun onResume() {
         super.onResume()
         binding!!.infoMap.onResume()
+        binding!!.apply{
+            infoName.text = parkArr[select!!].name
+            infoExpalinText.text = parkArr[select!!].explain
+        }
     }
 
     override fun onPause() {
@@ -69,7 +75,7 @@ class InfoFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
-        val myLocation = locArr[select!!]
+        val myLocation = parkArr[select!!].loc
         googleMap?.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
         googleMap?.moveCamera(CameraUpdateFactory.zoomTo(10.0f))
 
---
+++ AndroidApp/app/src/main/java/com/example/gardening/ParkData.kt
@@ -0,0 +1,5 @@
+package com.example.gardening
+
+import com.google.android.gms.maps.model.LatLng
+
+data class ParkData(val loc: LatLng, val name : String, val explain:String)
